4b4c96b1d84715b197c17c0cffc98d32f567288f
Dawid Podolak <dpodolak@pgs-soft.com>
2017-08-23 08:52:53 +0200

    MultiThreading

--- app/src/main/java/com/pgssoft/rxjava/Main.java
+++ app/src/main/java/com/pgssoft/rxjava/Main.java
@@ -18,9 +18,9 @@ class Main{
         UserProvider userProvider = new UserProvider(es);
 
 
-        Observable<String> stream1 = userProvider.getDelayedUser(100).map(user -> "Stream1: " + user.toString());
-        Observable<String> stream2 = userProvider.getDelayedUser(150).map(user -> "Stream2: " + user.toString());
-        Observable<String> stream3 = userProvider.getDelayedUser(200).map(user -> "Stream3: " + user.toString());
+        Observable<String> stream1 = userProvider.getDelayedUser(100).map(user -> "Stream1: " + user.toString()).subscribeOn(Schedulers.io());
+        Observable<String> stream2 = userProvider.getDelayedUser(150).map(user -> "Stream2: " + user.toString()).subscribeOn(Schedulers.io());
+        Observable<String> stream3 = userProvider.getDelayedUser(200).map(user -> "Stream3: " + user.toString()).subscribeOn(Schedulers.io());
 
 //        Observable<Observable<String>> streams = Observable.just(stream1, stream2, stream3);
 
